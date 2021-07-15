e365edf67c571aacae85b075eb2aae7b81cf43e6
torbjørn bratland <tobratland@pop-os.localdomain>
2021-07-15 16:03:42 +0200

    working on showing sim with wasm and js

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,6 +1,62 @@
 use wasm_bindgen::prelude::*;
+use lib_simulation as sim;
+use rand::prelude::*;
+use serde::Serialize;
 
 #[wasm_bindgen]
-pub fn whos_that_dog() -> String {
-    "Mister Peanutbutter".into()
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
+
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let sim = sim::Simulation::random(&mut rng);
+
+        Self { rng, sim }
+    }
+
+    pub fn world(&self) -> JsValue {
+        let world = World::from(self.sim.world());
+        JsValue::from_serde(&world).unwrap()
+    }
+}
+
+
+#[derive(Clone, Debug, Serialize)]
+pub struct World {
+    pub animals: Vec<Animal>,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Animal {
+    pub x: f32,
+    pub y: f32,
+    pub rotation: f32,
+}
+
+impl From<&sim::World> for World {
+    fn from(world: &sim::World) -> Self {
+        let animals = world
+            .animals()
+            .iter()
+            .map(Animal::from)
+            .collect();
+
+        Self { animals }
+    }
+}
+
+impl From<&sim::Animal> for Animal {
+    fn from(animal: &sim::Animal) -> Self {
+        Self {
+            x: animal.position().x,
+            y: animal.position().y,
+            rotation: animal.rotation().angle()
+        }
+    }
 }
--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,10 +1,9 @@
+use nalgebra as na;
+use rand::{Rng, RngCore};
+
 pub struct Simulation {
     world: World,
 }
-
-
-
-
 
 #[derive(Debug)]
 pub struct World {
@@ -14,17 +13,83 @@ pub struct World {
 
 #[derive(Debug)]
 pub struct Animal {
-    position: Point2,
+    position: na::Point2<f32>,
+    rotation: na::Rotation2<f32>, 
+    speed: f32,
 }
 
 
 #[derive(Debug)]
 pub struct Food {
-    position: Point2,
+    position: na::Point2<f32>,
 }
 
-#[derive(Debug)]
-pub struct Point2 {
-    x: f32,
-    y: f32,
+
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+
+    pub fn world(&self) -> &World {
+        &self.world
+    }
 }
+
+impl World {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        let animals = (0..40)
+            .map(|_| Animal::random(rng))
+            .collect();
+
+        let foods = (0..60)
+            .map(|_| Food::random(rng))
+            .collect();
+
+        
+        Self { animals, foods }
+    }
+
+    pub fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+
+    pub fn foods(&self) -> &[Food] {
+        &self.foods
+    }
+}
+
+
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+            rotation: rng.gen(),
+            speed: 0.002,
+        }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+
+    pub fn rotation(&self) -> na::Rotation2<f32> {
+        self.rotation
+    }
+}
+
+impl Food {
+    pub fn random(rng: &mut dyn RngCore)  -> Self {
+        Self {
+            position: rng.gen(),
+        }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+}
+
+    
